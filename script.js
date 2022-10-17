6926fea74a379ce1d0ecbaf3adecc390dd8244c2
Tom Jackson <tomcat468@gmail.com>
2022-10-17 17:54:41 +0100

    Create playRound function
    also, change console.log's in getComputerChoice to return.

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3)
     if (num==0) {
-        console.log("rock")
+        return "rock"
     } else if (num==1) {
-        console.log("paper")
+        return "paper"
     } else {
-        console.log("scissors")
+        return "scissors"
     }
 
 }
+function playRound (playerSelection, computerSelection) {
+
+    playerSelection.toLowerCase()
+
+    if (playerSelection===computerSelection) {
+        return "Tie!"
+    } else if (playerSelection=="rock" && computerSelection=="paper") {
+        return "You Lose!"
+    } else if (playerSelection=="paper" && computerSelection=="scissors") {
+        return "You Lose!"
+    } else if (playerSelection=="scissors" && computerSelection=="rock") {
+        return "You Lose"
+    } else {
+        return "You Win!"
+    }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
